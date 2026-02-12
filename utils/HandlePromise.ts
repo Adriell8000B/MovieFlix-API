c5bc31b5678668dbed5f48c277c77a09fb17f3fb
Adriell8000B <adrielluiz73@gmail.com>
2026-02-12 18:25:59 -0300

    refactor: update HandlePromise to accept PromiseLike type for improved flexibility

--- utils/HandlePromise.ts
+++ utils/HandlePromise.ts
@@ -1,4 +1,4 @@
-export async function HandlePromise<T>(promise: Promise<T>): Promise<[T | null, Error | null]> {
+export async function HandlePromise<T>(promise: PromiseLike<T>): Promise<[T | null, Error | null]> {
 	try {
 		const response = await promise
 		return [response, null]
